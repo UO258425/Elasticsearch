e5e04c8046662b727ca45bfca5ca1f61da658627
Carlos Manrique Enguita <UO258425@uniovi.es>
2019-12-15 19:58:18 +0100

    Work in progress

--- Ejercicio4/Exercise4ES-PoP.py
+++ Ejercicio4/Exercise4ES-PoP.py
@@ -1,6 +1,6 @@
 import simplejson
 import re
-
+from langdetect import detect
 
 def sortFreqDict(freqdict):
     aux = [(freqdict[key], key) for key in freqdict]
@@ -8,32 +8,64 @@ def sortFreqDict(freqdict):
     aux.reverse()
     return aux
 
+def printList(aList):
+    for i in aList:
+        print(i)
+
+
+#First we collect just the titles of the pop documents
 titles = []
 json_file = open('PoPAlcoholismComorbidity.json')
 popDocuments = simplejson.load(json_file)
 for paper in popDocuments:
-    for attribute, value in paper.iteritems():
+    for attribute, value in paper.items():
         if attribute == "title":
             titles.append(value)
 
+#printList(titles)
 
+#Now we remove all titles that are not written in english
+#Langdetect is not perfect and non deterministic, so sometimes values changes at
+# different executions.
+"""
+for title in titles:
+    if detect(title) != 'en':
+        titles.remove(title)
+"""
+#printList(titles)
 
-
+#We remove all english stopwords from the titles
 stopwords = [line.rstrip('\n') for line in open("EnglishStopWords.txt")]
 
-
+#Then we convert it to a list of words, and we remove duplicates
 wordList = []
 for i in titles:
     wordList.extend(re.sub("[^\w]", " ",  i).split())
 
-wordList = list( dict.fromkeys(wordList) ) #trick to remove duplicates
+#wordList = list( dict.fromkeys(wordList) ) #trick to remove duplicates
 
+for word in wordList:
+    try:
+        if detect(word) != 'en':
+            wordList.remove(word)
+    except: 
+        wordList.remove(word)
 
+#printList(wordList)
 
 wordfreq = [wordList.count(p) for p in wordList]
 wordfreq =  dict(zip(wordList,wordfreq))
 
 wordfreq= sortFreqDict(wordfreq)
 
-for i in wordfreq:
-    print i
+mostRelevantPoPWords = list()
+
+for entry in wordfreq:
+    try:
+        if entry[0] > 8:
+            mostRelevantPoPWords.append(entry[1])
+    except:
+        pass
+
+outputFile = open("popOutput.txt", "wt")
+outputFile.write("\n".join(mostRelevantPoPWords))
--- Ejercicio4/Exercise4ES.py
+++ Ejercicio4/Exercise4ES.py
@@ -3,7 +3,7 @@ from elasticsearch import Elasticsearch
 def main():
     es = Elasticsearch(["http://elastic.carlosmanrique.dev:9200"])
 
-    subredditsQuery = es.search( index="reddit-mentalhealth-stopwords-stemming-ngrams",
+    subredditsQuery = es.search( index="reddit-mentalhealth-stopwords-stemming",
                             body ={
       "size": 0,
       "query": {
@@ -30,12 +30,46 @@ def main():
 
 
     selectedSubreddits = list()
+    queryParam = ""
 
     for i in range(10):
         selectedSubreddits.append(subredditsQuery['aggregations']['Subreddits relevantes']['buckets'][i]['key'])
 
-    for i in selectedSubreddits:
-        print(i)
+    queryParam = selectedSubreddits[0]
+    for i in selectedSubreddits[2:]:
+        queryParam += ", "+i
+    
+    comorbiditiesQuery = es.search(
+        index="reddit-mentalhealth-stopwords-stemming",
+        body={
+            "size":0,
+            "query":{
+                "match":{
+                    "subreddit":{
+                        "query": queryParam,
+                        "operator":"or"
+                        }
+                    }
+                },
+            "aggs":{
+                "Most significant terms":{
+                    "significant_terms":{
+                        "field": "selftext",
+                        "size":1000,
+                        "gnd":{}
+                    }
+                }
+            }
+        })
+
+
+    popOutput = [line.rstrip("\n") for line in open("popOutput.txt")]
+    result = list()
+    for i in comorbiditiesQuery["aggregations"]["Most significant terms"]["buckets"]:
+        if i["key"] in popOutput:
+            result.append(i["key"])
+            print(i["key"])
+
 
         
 if __name__ == '__main__':
