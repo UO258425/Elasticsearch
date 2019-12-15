4d9e63711281b8eb5425b5e7399339878e2cf7be
Violeta <uo258454@uniovi.es>
2019-12-15 12:28:06 +0100

    Ejercicio 3 sin prints/comentarios

--- Exercise3/Exercise3.py
+++ Exercise3/Exercise3.py
@@ -10,12 +10,11 @@ import requests
 from elasticsearch import helpers
 
 def main():
-    # Queremos imprimir bonito
-    pp = pprint.PrettyPrinter(indent=2)
 
-    # Nos conectamos por defecto a localhost:9200
+
+    # Connect to our index
     es = Elasticsearch(['http://elastic.carlosmanrique.dev:9200'])
-    # En ocasiones las consultas tienen que formalizarse en JSON
+    # Index without stopwords
     results = es.search(
         index="reddit-mentalhealth-stopwords",
         body = {
@@ -28,7 +27,7 @@ def main():
                 }
             },
             "aggs":{
-                "Terminos mas significativos":{
+                "Significant terms":{
                     "significant_terms":{
                         "field":"selftext",
                         "size":100,
@@ -36,17 +35,27 @@ def main():
 
                         },
 
-                        "exclude":["prescribes", "prescribed", "prescribe","prescription","prescribing","prescriber", "medicine",
-                                   "meds", "drugs", "dose", "doctor", "pharmacy" , "diagnosed" ,"diagnostic",
-                                   "medications","medication","dose","dosage","pill","pills","psychiatrist","mg","tak*","effect","effects","symptoms","gp"
-                                   ,"antidepressants","drugs","lower","treatment","pharmacist","drug","doctors","height","infection","duration","lbs"
-                            ,"prescriptions","oral","nausea","fever","blood","intolerance","intolerances","appointment","appointments",
-                                   "addicted","addictive","addiction","tested","doses","adhd","doc","referred","allergy","allergic","tests"
-                            ,"specialist","reuptake","inflammation","grossly","recreational","recreationally","advised","advises","switched","experience",
-                                   "experienced","experiencing","side","effects","pain","pains","taking","took","taken","addictiveness","antidepressant"
-                                   ,"podiatrist","complaint","medical","emergency","urgent","attacks","smoker","result","results","recommended","recomendation",
-                                   "consultation","physician","physchaitrist","capsules","tablets","nuerologist","ssri","stimulant","stimulants","adverse","test"
-                                   ,"deficiency","decrease","decreased","caucasian","x","testing","fatigue","symptoms","dermatologist"]
+                        "exclude":["prescribes", "prescribed", "prescribe", "prescription", "prescribing", "prescriber",
+                                   "medicine",
+                                   "meds", "drugs", "dose", "doctor", "pharmacy", "diagnosed", "diagnostic",
+                                   "medications", "medication", "dose", "dosage", "pill", "pills", "psychiatrist", "mg",
+                                   "tak*", "effect", "effects", "symptoms", "gp"
+                            , "antidepressants", "drugs", "lower", "treatment", "pharmacist", "drug", "doctors",
+                                   "height", "infection", "duration", "lbs"
+                            , "prescriptions", "oral", "nausea", "fever", "blood", "intolerance", "intolerances",
+                                   "appointment", "appointments",
+                                   "addicted", "addictive", "addiction", "tested", "doses", "adhd", "doc", "referred",
+                                   "allergy", "allergic", "tests"
+                            , "specialist", "reuptake", "inflammation", "grossly", "recreational", "recreationally",
+                                   "advised", "advises", "switched", "experience",
+                                   "experienced", "experiencing", "side", "effects", "pain", "pains", "taking", "took",
+                                   "taken", "addictiveness", "antidepressant"
+                            , "podiatrist", "complaint", "medical", "emergency", "urgent", "attacks", "smoker",
+                                   "result", "results", "recommended", "recomendation",
+                                   "consultation", "physician", "physchaitrist", "capsules", "tablets", "nuerologist",
+                                   "ssri", "stimulant", "stimulants", "adverse", "test"
+                            , "deficiency", "decrease", "decreased", "caucasian", "x", "testing", "fatigue", "symptoms",
+                                   "dermatologist"]
 
 
 
@@ -57,40 +66,42 @@ def main():
         }
         )
 
-    # pp.pprint(results)
-    print(str(results["hits"]["total"]) + " resultados para una query \"rehab\"")
 
-    medications =[]
+    print(str(results["hits"]["total"]) + " potential medication terms")
+
+
     f = open("medications.txt", "wb")
     fPosts = open("significantTerms.txt","wb")
-    # Iteramos sobre los resultados, no es preciso preocuparse de las
-    # conexiones consecutivas que hay que hacer con el servidor ES
-    for hit in results["aggregations"]["Terminos mas significativos"]["buckets"]:
-        # get created date for a repo and fallback to authored_date for a commit
-        print(str(hit["key"]))
+    # Iterate through list of potential medications
+    for hit in results["aggregations"]["Significant terms"]["buckets"]:
         fPosts.write(hit["key"].encode("UTF-8"))
         fPosts.write("\n".encode("UTF-8"))
         entityQuery = "https://www.wikidata.org/w/api.php?action=wbsearchentities&search="+ hit["key"] +"&language=en&format=json"
+        # Get all of the Wikidata entities whose name matches the term
         r = requests.get(entityQuery).json()
         isMedication = False
+        #If it recovers any entity
         if len(r["search"])>0:
           for res in r["search"]:
-           #print(str(res['id']))
+
            code = str(res['id']);
            queryByCode = "https://www.wikidata.org/w/api.php?action=wbgetentities&ids="+code+"&languages=en&format=json"
+           #Get all of the Wikidata information of the entity with such code
            rCode = requests.get(queryByCode).json();
+           #If the entity has P31 wich is the "instanceof" property
+           #we go through all the all of them
            if "P31" in rCode["entities"][code]["claims"]:
             for code in rCode["entities"][code]["claims"]["P31"]:
+                #If this significant term hasn't found a medication match yet
+                #Avoids repeated data
                if not isMedication:
                 instanceCode = str(code["mainsnak"]["datavalue"]["value"]["id"]);
-                print(str(code["mainsnak"]["datavalue"]["value"]["id"]));
+               #The entity is an instance of pharmaceutical or product or medication
                 if instanceCode=="Q28885102" or instanceCode=="Q12140":
-                    print("Es un medicamento:" +str(hit["key"]))
+                    print("Is a medication:" +str(hit["key"]))
                     f.write(hit["key"].encode("UTF-8"))
                     f.write("\n".encode("UTF-8"))
                     isMedication=True
-
-
 
     f.close()
     fPosts.close()
